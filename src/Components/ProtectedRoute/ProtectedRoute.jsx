5fbb3b2fef8cf2b7bd2e37066a4e7f073d5e4821
Noureldeen-Elbialy <nourelbi292@gmail.com>
2024-04-15 20:48:26 +0200

    commit 4

--- src/Components/ProtectedRoute/ProtectedRoute.jsx
+++ src/Components/ProtectedRoute/ProtectedRoute.jsx
@@ -7,6 +7,6 @@ export default function ProtectedRoute(props) {
         return props.children;
     }
     else {
-        return <Navigate to={'/login'} />
+        return <Navigate to={'freshcart/login'} />
     }
 }
